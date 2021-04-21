cd4080a4f5d980fd299d4113ee0b1b20743f27d0
KB <katrinkajb@gmail.com>
2021-04-21 14:27:23 -0700

    if another user mentions someone being shamed it will auto-reply

--- index.js
+++ index.js
@@ -11,37 +11,33 @@ client.login(process.env.TOKEN);
 
 
 client.on('message', message => {
-    const rolesCount = message.guild.roles.cache.size;
     
-    const topOfRolesList = rolesCount - 1;
-
     if(!message.author.bot) {
+        if(message.content.toLowerCase().includes('!lockdown')) {
+            message.reply('you are on lockdown')
+            
+            const shamedAuthor = message.member.user.id;
 
-        let newRoleId = '';    
+            let isUserShamed = true;
 
-        message.guild.roles.create({ 
-            data: {
-                name: 'SHAME ROLE',
-                // creates a role just below the powerbot permission. Always has to be one step lower than the bot permission. Will need to set up bot permission in initial setup
-                position: topOfRolesList,
-                // Must be decimal
-                color: 6030083,
-                permissions: []
-            }
-        })
-        // finds id of newly created role
-        .then((result) => {
-            newRoleId = result.id;
-            message.member.roles.add(result);
-            message.reply('SHAME ON YOU');
-        })
+            client.setTimeout( () => {
+                message.guild.roles.cache.find
+                console.log('timeout complete');
+                message.reply('no longer shamed, rejoins society');
+                isUserShamed = false;
+            }, 10000);
+            
+            client.on('message', message => {
+                if(!message.author.bot && isUserShamed) {
+                    if (message.mentions.users.has(shamedAuthor)) { 
+                        message.reply('Sorry, this user is being shamed currently. Try again later')
+                    }
+                }
+            })
 
-        client.setTimeout( () => {
-            message.guild.roles.cache.find(role => role.id === newRoleId).delete();
-            console.log('timeout complete');
-            message.reply('You have been released from your shame');
-            }, 5000);
+        }
+                
+    
     }
 })
 
-// Won't work on server owner, need to figure out a different process for that case or owners only get light shame without losing any permissions
